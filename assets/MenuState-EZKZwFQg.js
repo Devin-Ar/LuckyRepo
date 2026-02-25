26804013e00cd225c36be2bde4f6153d56428f6c
Ayako <darroy@uw.edu>
2026-02-24 17:24:32 -0800

    Updates

---
+++ assets/MenuState-EZKZwFQg.js
@@ -0,0 +1,2 @@
+import{h as r}from"./index-BGo7J5MW.js";class o extends r{controller;constructor(t){super(),this.controller=t()}async init(){this.isRendering=!0,this.controller.init(this.name)}destroy(){this.controller.destroy()}}export{o as M};
+//# sourceMappingURL=MenuState-EZKZwFQg.js.map
