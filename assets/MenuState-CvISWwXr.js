07f70502463c9bcaed44af5e36fa84fdd1885883
Ayako <darroy@uw.edu>
2026-02-27 17:24:42 -0800

    Updates

---
+++ assets/MenuState-CvISWwXr.js
@@ -0,0 +1,2 @@
+import{h as r}from"./index-SB0jwsSG.js";class o extends r{controller;constructor(t){super(),this.controller=t()}async init(){this.isRendering=!0,this.controller.init(this.name)}destroy(){this.controller.destroy()}}export{o as M};
+//# sourceMappingURL=MenuState-CvISWwXr.js.map
