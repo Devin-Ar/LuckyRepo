08bad52e341f13772ee0821c5f71dce114b67a8f
David Norman <drnorm@uw.edu>
2026-03-14 22:05:48 -0700

    Updates

---
+++ assets/MenuState-Cj2o-0ex.js
@@ -0,0 +1,2 @@
+import{h as r}from"./index-t_snJT1D.js";class o extends r{controller;constructor(t){super(),this.controller=t()}async init(){this.isRendering=!0,this.controller.init(this.name)}destroy(){this.controller.destroy()}}export{o as M};
+//# sourceMappingURL=MenuState-Cj2o-0ex.js.map
